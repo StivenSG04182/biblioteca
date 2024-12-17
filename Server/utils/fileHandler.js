daf502d02efa6441ccdff7666d2bf42736c90fa3
StivenSG04182 <stivensanchez041821@gmail.com>
2024-12-16 22:29:38 -0500

    render 10

--- Server/utils/fileHandler.js
+++ Server/utils/fileHandler.js
@@ -1,7 +1,11 @@
 import path from 'path';
 import fs from 'fs/promises';
 import { pool as db } from '../config/database.js';
+import { fileURLToPath } from 'url';
 
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const UPLOAD_DIR = path.join(__dirname, '..', 'public', 'uploads');
 
